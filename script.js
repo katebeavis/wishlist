8a095dae4dc3ffa26f3f73ea6ade0d510140f123
katebeavis <katebeavis84@gmail.com>
2015-05-18 16:56:28 +0100

    Can mark items as done

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 /* Exercise 1: Wish list */
-function addToList(item) {
-  $("ol#items").append("<li>" + item + "</li>")
-}
 $(document).on("click", "#add-to-list", function(){
+
   var input = $("#item").val();
+
   addToList(input)
-  $("#item").val("");
+
+  $("#item").val("").focus();
 })
+
+$(document).on("click", "span.label.pending", function() {
+
+  var label = $(this).parent();
+
+  label.addClass("completed");
+
+  label.append('<span class="label success">Done!</span>');
+
+  $(this).remove();
+})
+
+function addToList(item) {
+
+  var span = '<span class="label pending">Pending</span>'
+
+  $("ol#items").append("<li>" + item + span + "</li>")
+}
