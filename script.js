d3e060312e6c8525041cb84447b5ecf4825d8d79
katebeavis <katebeavis84@gmail.com>
2015-05-18 16:06:15 +0100

    Added function and event listener for adding items to list

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 /* Exercise 1: Wish list */
+function addToList(item) {
+  $("ol#items").append("<li>" + item + "</li>")
+}
+$(document).on("click", "#add-to-list", function(){
+  var input = $("#item").val();
+  addToList(input)
+  $("#item").val("");
+})
